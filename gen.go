4b4505a3e2083ae593c6688c638167cc43091bdb
Kyle Shannon <kyle@pobox.com>
2021-04-14 09:30:23 -0600

    collapse yaml into one file

--- gen.go
+++ gen.go
@@ -4,16 +4,13 @@ import (
 	"encoding/json"
 	"flag"
 	"fmt"
-	"io"
 	"log"
 	"os"
-	"path/filepath"
-	"sort"
 
 	"github.com/go-yaml/yaml"
 )
 
-// A set of hetergeneous nodes
+// A set of homogeneous nodes
 type Nodes struct {
 	Type     string `json:"type" yaml:"type"`
 	Make     string `json:"make" yaml:"make"`
@@ -21,12 +18,13 @@ type Nodes struct {
 	CPU      string `json:"cpu" yaml:"cpu"`
 	CPUs     int    `json:"cpus" yaml:"cpus"`
 	CPUCores int    `json:"cpucores" yaml:"cpucores"`
-	GPU      string `json:"gpu,omitempty" yaml:"gpu"`
-	GPUs     int    `json:"gpus,omitempty" yaml:"gpus"`
-	GPUCores int    `json:"gpucores,omitempty" yaml:"gpucores"`
+	GPU      string `json:"gpu,omitempty" yaml:"gpu,omitempty"`
+	GPUs     int    `json:"gpus,omitempty" yaml:"gpus,omitempty"`
+	GPUCores int    `json:"gpucores,omitempty" yaml:"gpucores,omitempty"`
 	RAM      int    `json:"ram" yaml:"ram"` // GB
 	Count    int    `json:"count" yaml:"count"`
 	Owner    string `json:"owner" yaml:"owner"`
+	Added    string `json:"added" yaml:"added"`
 }
 
 type Cluster struct {
@@ -41,51 +39,27 @@ func main() {
 	flagFormat := flag.String("f", "json", "output format (json, yaml)")
 	flagPretty := flag.Bool("pretty", false, "pretty-print json")
 	flag.Parse()
-	var (
-		files []string
-		err   error
-	)
-	files, err = filepath.Glob("./*.yml")
+	r, err := os.Open("./borah.yml")
 	if err != nil {
 		log.Fatal(err)
 	}
-	sort.Slice(files, func(i, j int) bool {
-		if filepath.Base(files[i]) == "borah.yml" {
-			return true
-		} else if filepath.Base(files[j]) == "borah.yml" {
-			return false
-		}
-		return files[i] < files[j]
-	})
-
-	if files[0] != "borah.yml" {
-		panic("failed to sort files with borah first")
-	}
-
-	readers := make([]io.Reader, len(files))
-	for i, f := range files {
-		fin, err := os.Open(f)
-		if err != nil {
-			log.Fatal(err)
-		}
-		readers[i] = fin
-		defer fin.Close()
-	}
-	r := io.MultiReader(readers...)
 	var c Cluster
 	err = yaml.NewDecoder(r).Decode(&c)
 	if err != nil {
 		log.Fatal(err)
 	}
 	var b []byte
-	if *flagFormat == "json" {
+	switch *flagFormat {
+	case "json":
 		if *flagPretty {
 			b, err = json.MarshalIndent(c, "", "  ")
 		} else {
 			b, err = json.Marshal(c)
 		}
-	} else if *flagFormat == "yaml" {
+	case "yaml":
 		b, err = yaml.Marshal(c)
+	default:
+		log.Fatalf("invalid format: %s", *flagFormat)
 	}
 	if err != nil {
 		log.Fatal(err)
